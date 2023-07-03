841340626cb743887804372a692ddae35e86c30d
Nandasudesh <121796168+Nandasudesh@users.noreply.github.com>
2023-07-03 18:50:36 +0530

    Test update

--- Sample_Jenkins/src/main/java/jenkins_Test/Test.java
+++ Sample_Jenkins/src/main/java/jenkins_Test/Test.java
@@ -1,24 +1,17 @@
 package jenkins_Test;
 
-import java.util.Scanner;
+
 
 public class Test {
 	public static void main(String[] args) {
-		int sum=0;
-		System.out.println("Enetr the number to check");
-		Scanner sc=new Scanner(System.in);
-		int no=sc.nextInt();
-		int temp=no;
+		int no=123456789;
+		int count=0;
+		int r;
 		while(no>0) {
-			int r=no%10;
-			sum=sum+(r*r*r);
 			no=no/10;
-			
+			count++;
 		}
-if (sum==temp) {
-	System.out.println(temp+ " is Armstrong number");
-}
-else {System.out.println(temp+ " is not Armstrong number");
-}
+		System.out.println(count);
+
 	}
 }
